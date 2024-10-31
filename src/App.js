14b87ebe75e10580043bea863482607f6733b60b
Dhanalakshmi <dhana01898@gmail.com>
2024-10-30 20:20:13 -0400

    path changes

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ export default function App() {
     <div className="App">
       <Navbar />
       <List /> 
+      <MultiStepForm />  
        <BrowserRouter>  
       <Routes>
         <Route path="/" element={<List />}> </Route>
